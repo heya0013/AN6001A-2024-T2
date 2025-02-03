67ac78feb0c92c47d1aff39380f1cbb493b81b71
Davis#goodnight <HEYA0013@e.ntu.edu.sg>
2025-02-03 07:24:37 +0000

    1

--- app.py
+++ app.py
@@ -1,32 +1,36 @@
-#
-#Syn code spaces is very high risk,someyimes fail
- 
-from flask import Flask,request,render_template
-import textblob
+genai.configure(api_key=api)
+model = genai.GenerativeModel("gemini-1.5-flash")
 
 app = Flask(__name__)
 
 @app.route("/",methods=["GET","POST"])
 def index():
-    if request.method == "POST":
-        a = request.form['options']
-        print(a)
     return(render_template("index.html"))
 
 @app.route("/main",methods=["GET","POST"])
 def main():
-    name=request.form.get("q")
+    name = request.form.get("q")
     return(render_template("main.html"))
 
 @app.route("/SA",methods=["GET","POST"])
 def SA():
     return(render_template("SA.html"))
-    
+
 @app.route("/SA_result",methods=["GET","POST"])
 def SA_result():
-    q=request.form.get("q")
-    r= r=textblob.TextBlob(q).sentiment
+    q = request.form.get("q")
+    r = textblob.TextBlob(q).sentiment
     return(render_template("SA_result.html",r=r))
+
+@app.route("/genAI",methods=["GET","POST"])
+def genAI():
+    return(render_template("genAI.html"))
+
+@app.route("/genAI_result",methods=["GET","POST"])
+def genAI_result():
+    q = request.form.get("q")
+    r = model.generate_content(q)
+    return(render_template("genAI_result.html",r=r.candidates[0].content.parts[0].text))
 
 if __name__ == "__main__":
     app.run()
